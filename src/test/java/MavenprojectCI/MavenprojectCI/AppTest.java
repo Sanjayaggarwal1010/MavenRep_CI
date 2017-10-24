a0647809f6984db446323263fe5bbd338e789874
megha <megha@MJ>
2017-10-24 19:50:59 +0530

    commit

--- src/test/java/MavenprojectCI/MavenprojectCI/AppTest.java
+++ src/test/java/MavenprojectCI/MavenprojectCI/AppTest.java
@@ -48,6 +48,7 @@ public class AppTest
 			System.out.println("End of Method");
 			System.out.println("End of Method- Just -22/10/2017");
 			System.out.println("End of Method- Just -26/10/2017");
+			System.out.println("End of Method- Just -27/10/2017");
 		}
 	 @Test
 	 public void seleniumtest2()
